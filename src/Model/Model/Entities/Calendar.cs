e38615861019ad61931f8d12a2e20af26e7c0b56
Nicolás Escalante <nlante@gmail.com>
2013-07-18 20:23:23 -0300

    modelo y fix en los packages

---
+++ src/Model/Model/Entities/Calendar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Model
+{
+    public class Calendar : AbstractUpdatableClass
+    {
+        public int? StartTime { get; set; }
+
+        public int? EndTime { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public bool? OnMonday { get; set; }
+
+        public bool? OnTuesday { get; set; }
+
+        public bool? OnWednesday { get; set; }
+
+        public bool? OnThursday { get; set; }
+
+        public bool? OnFriday { get; set; }
+
+        public bool? OnSaturday { get; set; }
+
+        public bool? OnSunday { get; set; }
+    }
+}
